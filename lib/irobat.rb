65f0a0b39707f96e1f439d638db990dcdf8c27ca
Joseph Wilk <joe@josephwilk.net>
2009-07-09 10:58:58 -0400

    Adding Readme + tweaking sinatra servers with more international names

--- lib/irobat.rb
+++ lib/irobat.rb
@@ -12,14 +12,14 @@ module IRobat
   SERVER_DIR = File.dirname(__FILE__) + '/server/'
 
   def self.startup_iphone_coordinators
-    puts "Starting coach"
-    Kernel.fork {`cd #{SERVER_DIR};ruby coach.rb -p 3001 2> /dev/null`}
-    puts "Starting quarterback"
-    Kernel.fork {`cd #{SERVER_DIR};ruby quarterback.rb -p 3000 2> /dev/null`}
+    puts "Starting iphone instructor..."
+    Kernel.fork {`cd #{SERVER_DIR};ruby instructor.rb -p 3001 2> /dev/null`}
+    puts "Starting iphone receiver..."
+    Kernel.fork {`cd #{SERVER_DIR};ruby receiver.rb -p 3000 2> /dev/null`}
     sleep(2)
   end
   
-  def self.shutdown_iphone_coordinators
+  def self.shutdown_iphone_coach
     Curl::Easy.http_post 'http://localhost:3001/', 'q=' + CGI.escape(Phone::Command.empty)
   end
   
@@ -28,5 +28,5 @@ end
 IRobat.startup_iphone_coordinators
 
 at_exit {
-  IRobat.shutdown_iphone_coordinators
+  IRobat.shutdown_iphone_coach
 }  
