24b361b91d952772ddde22b74e8fa612280e00a2
Joseph Wilk <joe@josephwilk.net>
2009-07-09 12:35:08 -0400

    Add defaults for fields

--- lib/irobat/iphone_world.rb
+++ lib/irobat/iphone_world.rb
@@ -4,17 +4,20 @@ module IRobat
   module IPhoneWorld
     def self.method_missing(method, *args)
       @phone ||= PhoneDriver.new
-    
+
       @phone.send(method, *args)
     end
 
-    def self.touch_table_row(field, value)
+    def self.touch_table_row(value, field = 'text')
       simulate_touch ['className', 'UITableViewCell', field, value]
     end
 
-    def self.touch_button(field, value)
+    def self.touch_button(value, field = 'currentTitle')
       simulate_touch ['className', 'UIRoundedRectButton', field, value]
-      
+    end
+
+    def self.check_label_contains(value, field = 'text')
+      check_match_count ['className', 'UILabel', field, value],['matchCount', 1]
     end
   end
 end
